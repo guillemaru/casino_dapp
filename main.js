ef3130a8ef5f61a8fd552a90b629e861c9addc7e
guillemaru <multiguillermo@hotmail.com>
2021-07-25 01:28:05 +0200

    Finish part 2

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
-Moralis.initialize("APPLICATION_ID"); // Application id from moralis.io
-Moralis.serverURL = "SERVER_URL"; //Server url from moralis.io
+Moralis.initialize("vyadCP6fproZTRanB3C0NNQOdmf4wyUnSk8v3sar"); // Application id from moralis.io
+Moralis.serverURL = "https://9eas7bxogeyv.usemoralis.com:2053/server"; //Server url from moralis.io
+
+function addRowToTable(tableId, data) {
+    let tableRow = document.createElement('tr');
+    data.forEach(element=> {
+        let newColumn = document.createElement('td');
+        newColumn.innerHTML = element;
+        tableRow.appendChild(newColumn);
+    });
+    document.getElementById(tableId).appendChild(tableRow);
+}
 
 async function login() {
     try {
@@ -8,9 +18,23 @@ async function login() {
             user = await Moralis.Web3.authenticate();
         } 
         console.log(user);
-        alert("User logged in")
         document.getElementById("login_button").style.display= "none";
         document.getElementById("game").style.display= "block";
+
+        let biggestWinners = await Moralis.Cloud.run("biggestWinners", {});
+        biggestWinners.forEach( (row) =>{
+            addRowToTable("top_winners", [row.objectId,row.total_sum]);
+        });
+        
+        let biggestLosers = await Moralis.Cloud.run("biggestLosers", {});
+        biggestLosers.forEach( (row) =>{
+            addRowToTable("top_losers", [row.objectId,row.total_sum]);
+        });
+        
+        let biggestBets = await Moralis.Cloud.run("biggestBets", {});
+        biggestBets.forEach( (row) =>{
+            addRowToTable("biggest_bets", [row.user,row.bet, row.win]);
+        });
     } catch (error) {
         console.log(error);
     }
@@ -19,7 +43,7 @@ async function login() {
 async function flip(side) {
     let amount = document.getElementById("amount").value; 
     window.web3 = await Moralis.Web3.enable();
-    let contractInstance = new web3.eth.Contract(window.abi,"0x9624c187a827C8C3b1A2186da8E9c88c98426eC5")
+    let contractInstance = new web3.eth.Contract(window.abi,"0x5838d2C4Dc6CeBbc9521b643B4d80584E6bc71d6")
     contractInstance.methods.flip(side == "heads" ? 0 : 1).send({value: amount, from: ethereum.selectedAddress})
     .on('receipt',function(receipt){
         console.log(receipt);
@@ -32,6 +56,12 @@ async function flip(side) {
     })
 }
 
+async function logout(){
+    await Moralis.User.logOut();
+    alert("logged out")
+}
+
 document.getElementById("login_button").onclick = login;
+document.getElementById("logout_button").onclick = logout;
 document.getElementById("heads_button").onclick = function(){flip("heads")};
 document.getElementById("tails_button").onclick = function(){flip("tails")};
